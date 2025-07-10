173d82136b336c6bf807a148e9c82224ece93ead
Brian Crowe <brian.j.crowe@protonmail.com>
2025-07-09 23:37:37 -0400

    syntax update, add random color picker to elements for fun

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
+const randomNumber = () => Math.floor(Math.random() * 256)
+
+const randomColorPicker = () => {
+  let r = randomNumber()
+  let g = randomNumber()
+  let b = randomNumber()
+  return `rgb(${r}, ${g}, ${b})`
+}
+
 // 1: Select the element with the ID 'about'.
 //    Store the element in the variable `about`.
-const about = document.querySelector('#about')
+const about = document.getElementById('about')
 about.style.border = '2px solid firebrick'
 
 // 2: Select all the <h2> elements in the document.
 //    Set the color of the <h2> elements to a different color.
 const h2Elements = document.querySelectorAll('h2')
 h2Elements.forEach((h2) => {
-  h2.style.color = 'darkcyan'
+  h2.style.color = randomColorPicker()
 })
 
 // 3: Select all elements with the class '.card'.
 //    Set their background color to the color of your choice.
 const cardElements = document.querySelectorAll('.card')
 cardElements.forEach((card) => {
-  card.style.backgroundColor = 'lightskyblue'
+  card.style.backgroundColor = randomColorPicker()
 })
 
 // 4: Select only the first <ul> in the document.
@@ -30,15 +39,6 @@ container.style.backgroundColor = 'royalblue'
 // 6: Select all <a> elements that have a 'title' attribute.
 //    Set their color value to the color of your choice.
 const linkTitles = document.querySelectorAll('a[title]')
-
-const randomNumber = () => Math.floor(Math.random() * 256)
-
-const randomColorPicker = () => {
-  let r = randomNumber()
-  let g = randomNumber()
-  let b = randomNumber()
-  return `rgb(${r}, ${g}, ${b})`
-}
 
 linkTitles.forEach((link) => {
   link.style.color = randomColorPicker()
